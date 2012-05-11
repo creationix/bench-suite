3e2ea74b9ae94aa3af555d48cfd2fb66f8b5d11f
Tim Caswell <tim@creationix.com>
2012-05-11 17:16:47 -0500

    Echo the input strings to test

--- db.c
+++ db.c
@@ -41,6 +41,7 @@ uv_buf_t on_alloc(uv_handle_t* handle, size_t suggested_size) {
 void on_read(uv_stream_t* stream, ssize_t nread, uv_buf_t buf) {
 /*  client_t* client = stream->data;
 */
+  printf("on_read %.*s\n", (int)buf.len, buf.base);
   /* Handle data */
 
   free(buf.base);
